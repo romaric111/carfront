4181326ba4ae60db5978d9f9993ee0061b0ae24e
romaric111 <romaric.hyacinthe@gmail.com>
2026-03-26 19:02:17 -0300

    Adding delete component

--- src/api/carapi.ts
+++ src/api/carapi.ts
@@ -5,3 +5,8 @@ export const getCars = async (): Promise<CarResponse[]> => {
   const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`);
   return response.data._embedded.cars;
 };
+
+export const deletecar = async (link: string): Promise<CarResponse> => {
+  const response = await axios.delete(link);
+  return response.data;
+};
--- src/components/Carlist.tsx
+++ src/components/Carlist.tsx
@@ -1,21 +1,55 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCars } from "../api/carapi";
-import { DataGrid, type GridColDef } from "@mui/x-data-grid";
-
-const columns: GridColDef[] = [
-  { field: "brand", headerName: "Brand", width: 200 },
-  { field: "model", headerName: "Model", width: 200 },
-  { field: "color", headerName: "Color", width: 200 },
-  {
-    field: "registrationNumber",
-    headerName: "Registration Number",
-    width: 150,
-  },
-  { field: "modelYear", headerName: "Model Year", width: 150 },
-  { field: "price", headerName: "Price", width: 150 },
-];
+import {
+  DataGrid,
+  type GridColDef,
+  type GridCellParams,
+} from "@mui/x-data-grid";
+import Snackbar from "@mui/material/Snackbar";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getCars, deletecar } from "../api/carapi";
+import { useState } from "react";
 
 function Carlist() {
+  const [open, setOpen] = useState(false);
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: deletecar,
+    onSuccess: () => {
+      setOpen(true);
+      //Car deleted, refetch the cars
+      queryClient.invalidateQueries({ queryKey: ["cars"] });
+    },
+    onError: (err) => {
+      console.error(err);
+    },
+  });
+
+  const columns: GridColDef[] = [
+    { field: "brand", headerName: "Brand", width: 200 },
+    { field: "model", headerName: "Model", width: 200 },
+    { field: "color", headerName: "Color", width: 200 },
+    {
+      field: "registrationNumber",
+      headerName: "Registration Number",
+      width: 150,
+    },
+    { field: "modelYear", headerName: "Model Year", width: 150 },
+    { field: "price", headerName: "Price", width: 150 },
+
+    {
+      field: "delete",
+      headerName: "",
+      width: 90,
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: (params: GridCellParams) => (
+        <button onClick={() => mutate(params.row._links.car.href)}>
+          Delete
+        </button>
+      ),
+    },
+  ];
+
   const { data, error, isLoading } = useQuery({
     queryKey: ["cars"],
     queryFn: getCars,
@@ -29,11 +63,20 @@ function Carlist() {
     return <span>Error when fetching cars...</span>;
   } else {
     return (
-      <DataGrid
-        rows={data}
-        columns={columns}
-        getRowId={(row) => row._links.self.href}
-      />
+      <>
+        <DataGrid
+          rows={data}
+          columns={columns}
+          disableRowSelectionOnClick={true}
+          getRowId={(row) => row._links.self.href}
+        />
+        <Snackbar
+          open={open}
+          autoHideDuration={3000}
+          onClose={() => setOpen(false)}
+          message="Car deleted successfully"
+        />
+      </>
     );
   }
 }
